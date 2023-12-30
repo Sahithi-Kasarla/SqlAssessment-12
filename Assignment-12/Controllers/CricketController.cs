90ecdf3447a7291bf3125dcda63989a0b3c50188
Sahithi-kasarla <sahithikasarla123@gmail.com>
2023-12-30 12:59:43 +0530

    SQLAssignment-12

---
+++ Assignment-12/Controllers/CricketController.cs
@@ -0,0 +1,47 @@
+﻿using Assignment_12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_12.Controllers
+{
+    public class CricketController : Controller
+    {
+        
+        // GET: Cricket
+        static List<Cricket> listCricketTeams = new List<Cricket>()
+     {
+     new Cricket() { TeamId =1,TeamName="india",NOWC=2},
+    new Cricket() { TeamId =2,TeamName="Australia",NOWC=4},
+    new Cricket() { TeamId =3,TeamName="Weat Indies",NOWC=2},
+    new Cricket() { TeamId =4,TeamName="England",NOWC=1},
+     };
+        // GET: Cricket
+        [HttpGet]
+        public ActionResult create()
+        {
+            return View(new Cricket());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Cricket team)
+        {
+            if (ModelState.IsValid)
+            {
+                listCricketTeams.Add(team);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Index()
+        {
+            return View(listCricketTeams);
+        }
+        public ActionResult Display()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ Assignment-12/Models/Cricket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_12.Models
+{
+    public class Cricket
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+    }
+}
